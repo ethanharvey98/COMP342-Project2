4289fdf86f48753a06cba153a03624c7adc886f0
ethanharvey98 <43649122+ethanharvey98@users.noreply.github.com>
2019-11-03 19:07:19 -0500

    Add files via upload

--- FTPClient.py
+++ FTPClient.py
@@ -1,4 +1,99 @@
-# Ethan Harvey
-# FTPClient.py
-
 import socket
+import os
+    
+# port number
+port = 2222
+client_socket = socket.socket()
+
+# get IP Address
+ipAddress = input("Enter a host name (URL) or IP address for the FTP server: ")
+
+try:
+    client_socket.connect((ipAddress, int(port)))
+    # print welcome message
+    print("Welcome to GCC FTP client!")
+    print()
+
+    # takes first command
+    command = input("Command>> ")
+    cmd = command.split(" ")
+
+    while cmd[0].lower().strip() != 'quit':
+
+        # LIST command
+        if cmd[0].lower().strip() == 'list':
+            # send command
+            client_socket.send(bytes(command,'utf-8'))
+            # get data
+            data = client_socket.recv(1024).decode()
+            # print data
+            print(" FS: " + data)
+
+        # RETR command
+        if cmd[0].lower().strip() == 'retr':
+            # send command
+            client_socket.send(bytes(command,'utf-8'))
+            # save filename
+            filename = cmd[1]
+            # check to see if the file exists
+            response = client_socket.recv(1024).decode()
+            # if file exists
+            if (response == "True"):
+                file = open(filename,'wb')
+                while True:
+                    try:
+                        client_socket.settimeout(3.0)
+                        data = client_socket.recv(1024)
+                    except:
+                        client_socket.settimeout(None)
+                        break
+                    file.write(data)
+                file.close()
+                print(" FS: " + filename + " retrieved")
+            # if the file doesn't exist
+            else:
+                print(" FS: " + filename + " does not exist")
+
+        # STOR command
+        if cmd[0].lower().strip() == 'stor':
+            # send command
+            client_socket.send(bytes(command,'utf-8'))
+            try:
+                filename = cmd[1]
+                file = open(filename,"rb")
+                client_socket.send("True".encode('utf-8'))
+                data = file.read(1024)
+                client_socket.send(data)
+                while data:
+                    data = file.read(1024)
+                    client_socket.send(data)
+                file.close()
+                response = client_socket.recv(1024).decode()
+                if (response == "True"):
+                    print(" FS: " + filename + " stored")
+            except:
+                client_socket.send("False".encode('utf-8'))
+                print(" FS: " + filename + " does not exist")
+
+        # SIZE command
+        if cmd[0].lower().strip() == 'size':
+            # send command
+            client_socket.send(bytes(command,'utf-8'))
+            # get data
+            data = client_socket.recv(1024).decode()
+            # print data
+            print(" FS: " + data)
+
+        # get next command
+        command = input("Command>> ")
+        cmd = command.split(" ")
+
+    # close the connection
+    client_socket.send(bytes(command,'utf-8'))
+    client_socket.close()
+    print('Connection terminated')
+
+
+except:
+    # failed to connect
+    print("Failed to connect")
--- FTPServer.py
+++ FTPServer.py
@@ -1,19 +1,84 @@
-# Ethan Harvey
-# FTPServer.py
+import socket, threading
+import os
 
-import socket
+# print welcome message
+print("Welcome to GCC FTP service!")
+print("Waiting for client commands..")
+# ipaddress
+ipAddress = "10.23.102.47"
+# port number
+port = 2222
+server = socket.socket()
+server.bind((ipAddress, port))
 
-HOST = '127.0.0.1'
-PORT = 2222
+while True:
+    server.listen(1)
+    client_socket, clientAddress = server.accept()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
+    cmd = ''
+    while True:
+        cmd = ''
+        command = client_socket.recv(1024).decode()
+        # breaks command into array of strings
+        cmd = command.split(' ')
+
+        if (len(command)>0):
+
+            # LIST command
+            if(cmd[0]=='list'):
+                files = os.listdir(os.curdir)
+                smg = ""
+                for f in files:
+                    smg = smg + "\n " + f
+                client_socket.send(smg.encode('utf-8'))
+
+            # RETR command
+            elif(cmd[0]=='retr'):
+                try:
+                    filename = cmd[1]
+                    file = open(filename,"rb")
+                    client_socket.send("True".encode('utf-8'))
+                    data = file.read(1024)
+                    client_socket.send(data)
+                    while data:
+                        data = file.read(1024)
+                        client_socket.send(data)
+                    file.close()
+                except:
+                    client_socket.send("False".encode('utf-8'))
+
+                    
+            # STOR command
+            elif(cmd[0]=='stor'):
+                filename = cmd[1]
+                # make sure file exists
+                response = client_socket.recv(1024).decode()
+                # if file exists
+                if (response == "True"):
+                    file = open(filename,'wb')
+                    while True:
+                        try:
+                            client_socket.settimeout(3.0)
+                            data = client_socket.recv(1024)
+                        except:
+                            client_socket.settimeout(None)
+                            break
+                        file.write(data)
+                    client_socket.send("True".encode('utf-8'))
+                    file.close()
+                
+
+            # SIZE command
+            elif(cmd[0]=='size'):
+                reqFile = cmd[1]
+                statinfo = os.stat(reqFile)
+                size = str(statinfo.st_size) + " bytes"
+                client_socket.send(size.encode('utf-8'))
+
+            # QUIT command
+            elif (cmd[0]=="quit"):
+                client_socket.close()
                 break
-            conn.sendall(data)
+
+    # if client quits
+    print("Connection terminated by client...")
